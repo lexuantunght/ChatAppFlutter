cc0200a0e19d0a9ad41474b5aa6f465f936578d8
XuanTung <lexuantunght@gmail.com>
2021-06-09 13:30:01 +0700

    bai thuc hanh 3 se346

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/xuantung/Documents/UIT/SE346-Mobile/Flutter/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/xuantung/Documents/UIT/SE346-Mobile/BTH3/flutter_chat_app"
+export "FLUTTER_TARGET=/Users/xuantung/Documents/UIT/SE346-Mobile/BTH3/flutter_chat_app/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=flutter.inspector.structuredErrors%3Dtrue"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/xuantung/Documents/UIT/SE346-Mobile/BTH3/flutter_chat_app/.dart_tool/package_config.json"
